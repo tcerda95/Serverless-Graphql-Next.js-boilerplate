dd5f7e2572d3b32ab6cb97983fcb9b5d99ec0c43
tcerdaITBA <tcerda@itba.edu.ar>
2019-01-06 16:40:28 -0300

    chore(web): refactor Header to classfull component

--- web/components/Layout/Header.jsx
+++ web/components/Layout/Header.jsx
@@ -1,29 +1,37 @@
+import { Component } from 'react';
 import Link from 'next/link';
 import { withRouter } from 'next/router';
 import { Tab, Links, LinkItem, A } from './styles';
 import { withUser } from '../../lib/auth';
 
-const NextLink = ({ href, children, currentPathname }) => (
-  <LinkItem>
-    <Link prefetch href={href}>
-      <A active={href === currentPathname}>{children}</A>
-    </Link>
-  </LinkItem>
-);
+class Header extends Component {
+  NextLink = ({ href, label }) => {
+    const { pathname } = this.props.router;
 
-const Header = ({ router, user }) => {
-  const { pathname } = router;
-  const sign = user ? { href: '/signout', text: 'Sign out' } : { href: '/signin', text: 'Sign in' };
+    return (
+      <LinkItem>
+        <Link prefetch href={href}>
+          <A active={href === pathname}>{label}</A>
+        </Link>
+      </LinkItem>
+    );
+  }
 
-  return (
-    <Tab>
-      <Links>
-        <NextLink href="/" currentPathname={pathname}>Posts</NextLink>
-        {user && <NextLink href="/newpost" currentPathname={pathname}>New post</NextLink>}
-        <NextLink href={sign.href} currentPathname={pathname}>{sign.text}</NextLink>
-      </Links>
-    </Tab>
-  );
-};
+  render() {
+    const { user } = this.props;
+    const { NextLink } = this;
+    const sign = user ? { href: '/signout', label: 'Sign out' } : { href: '/signin', label: 'Sign in' };
+
+    return (
+      <Tab>
+        <Links>
+          <NextLink href="/" label="Posts" />
+          {user && <NextLink href="/newpost" label="New post" />}
+          <NextLink href={sign.href} label={sign.label} />
+        </Links>
+      </Tab>
+    );
+  }
+}
 
 export default withRouter(withUser(Header));
