6e0bd773955e667771347f605a19d574ef25ff32
tcerdaITBA <tcerda@itba.edu.ar>
2019-01-09 11:45:09 -0300

    fix(web): env-config production api url

--- web/env-config.js
+++ web/env-config.js
@@ -1,5 +1,7 @@
 const prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  'process.env.API_URL': prod ? 'https://api.example.com' : 'http://localhost:4000'
+  'process.env.API_URL': prod
+    ? 'https://x6qc7fuiua.execute-api.sa-east-1.amazonaws.com/prod/'
+    : 'http://localhost:4000'
 };
